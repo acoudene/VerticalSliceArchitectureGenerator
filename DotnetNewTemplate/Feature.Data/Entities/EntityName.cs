20a6a0494f303aee5801fea925fe9c616cdc3040
Anthony COUDENE <anthony.coudene@technidata-web.com>
2024-02-15 21:44:14 +0100

    Add comments to define base entity if inheritance

--- DotnetNewTemplate/Feature.Data/Entities/EntityName.cs
+++ DotnetNewTemplate/Feature.Data/Entities/EntityName.cs
@@ -3,9 +3,18 @@
 
 namespace Feature.Data.Entities;
 
+// This commented part could be used to have benefits of entity typing
+//public abstract record EntityNameBase : IIdentifierEntity
+
 public record EntityName : IIdentifierEntity
 {
   public required Guid Id { get; set; }
 
   // TODO - EntityProperties - Fields to complete
 }
+
+// This commented part could be used to have benefits of entity typing
+// Example of inherited class
+//public record EntityNameInherited : EntityNameBase
+//{
+//}
