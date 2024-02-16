17b3fb1c48710c957c5a6fee7a5eba806022c9bd
Anthony COUDENE <anthony.coudene@technidata-web.com>
2024-02-16 21:10:25 +0100

    Correct issue on inheritance : https://github.com/dotnet/aspnetcore/issues/44852

--- DotnetNewTemplate/Feature.Dtos/EntityNameDto.cs
+++ DotnetNewTemplate/Feature.Dtos/EntityNameDto.cs
@@ -4,6 +4,7 @@
 namespace Feature.Dtos;
 
 // This commented part could be used to have benefits of json entity typing
+//[JsonPolymorphic]
 //[JsonDerivedType(typeof(EntityNameInheritedDto), EntityNameInheritedDto.TypeId)]
 public record EntityNameDto : IIdentifierDto
 {
@@ -14,6 +15,7 @@ public record EntityNameDto : IIdentifierDto
 
 // This commented part could be used to have benefits of json entity typing
 // Example of inherited class
+//[JsonDerivedType(typeof(EntityNameInheritedDto), EntityNameInheritedDto.TypeId)]
 //public record EntityNameInheritedDto : EntityNameDtoBase
 //{
 //  public const string TypeId = "entityName.entityNameInherited";
