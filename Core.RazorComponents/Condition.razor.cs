db850a0c60f643a53ff9ce609cf7cdc2862219d0
Anthony COUDENE <anthony.coudene@technidata-web.com>
2024-09-11 16:46:39 +0200

    Add Core.RazorComponents and integrate Condition and ForEach components

---
+++ Core.RazorComponents/Condition.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Core.RazorComponents;
+
+public partial class Condition : ComponentBase
+{
+  [Parameter]
+  public bool Predicate { get; set; }
+
+  [Parameter]
+  public RenderFragment? ChildContent { get => Then; set => Then=value; }
+
+  [Parameter]
+  public RenderFragment? Then { get; set; }
+
+  [Parameter]
+  public RenderFragment? Else { get; set; }
+}
---
+++ Core.RazorComponents/ForEach.razor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Core.RazorComponents;
+
+public partial class ForEach<T> : ComponentBase
+{
+  [Parameter]
+  public IEnumerable<T>? Items { get; set; }
+
+  [Parameter]
+  public RenderFragment<T>? ChildContent { get; set; }
+}
