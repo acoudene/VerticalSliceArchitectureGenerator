bcfa08527a268e86df10b3906a4af5539639f6ee
Anthony COUDENE <anthony.coudene@technidata-web.com>
2024-10-03 18:17:09 +0200

    Add Mongo container to .Net Aspire

--- DotnetNewTemplate/Feature.AppHost/Program.cs
+++ DotnetNewTemplate/Feature.AppHost/Program.cs
@@ -1,5 +1,12 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+// Temporary independant Mongo coupled only by port and database name but a coupling will be done.
+const int port = 27017;
+const string databaseName = "feature";
+
+var mongoContainer = builder.AddMongoDB("mongo", port);
+mongoContainer.AddDatabase(databaseName);
+
 builder.AddProject<Projects.Feature_Host>("feature-host");
 
 builder.AddProject<Projects.Feature_WebApp>("feature-webapp");
